a6b22acea66669671f3bea6b89a833faaea842ef
Sherrif666 <136990169+Sherrif666@users.noreply.github.com>
2023-06-19 21:21:32 +0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+print("""----------/\
+                   / \
+         -------------
+            |  PyVirus|
+            |         |
+            |         |
+            |=========| 0.1
+            ===========
+            DEV:Sherrif666
+            TG: """)
+            
+input("введите жертву>>>>"))
+for i in range(10000):
+	print("PYVirus😎😎")
+def Virus_javascript():
+	Virus=True
+	Virus.run()
+	Virus.Number()
+	random=Number
+	if Virus:
+		Virus(999)
+	
