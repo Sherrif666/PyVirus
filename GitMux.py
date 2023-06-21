62c5610b9041b004f3ad39739b47c9eac92eeb6e
Sherrif666 <136990169+Sherrif666@users.noreply.github.com>
2023-06-21 21:39:39 +0600

    Add files via upload

---
+++ GitMux.py
@@ -0,0 +1,17 @@
+print("""/  \   /\
+     />>>>\>/>>\>>>>>:
+|>>>>>>>>>>>>>>GitMux>>>>>>>|   	
+    	DEV:@Sherrif666
+     	TG:Sherrif Code
+     	Github:Sherrif666""")
+input("GitHub>>>")  
+if 6>0:
+	for i in range(6000):
+		print("GIT Bomber")
+Github=True
+def Github_Main():
+	Github.run()
+	Github.input("cd GitMux>>")
+	Github.file('./git.java')
+	file.run()
+	file.MainLoop()
---
+++ git.java
@@ -0,0 +1,12 @@
+Github=true//Github//
+
+private files
+var files=".git"   public class python{
+	String=files
+	import java
+	
+	class Github:
+	if(5>10){
+		System.run()
+		GitMux=URL
+		var URL="github.com" git=false
